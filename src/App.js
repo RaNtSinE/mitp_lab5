2cb36e5d7eb8b03337d7573038df5a25bf353b9e
RaNtSinE <rantsine@mail.ru>
2021-12-03 09:18:27 +0700

    fix first focus

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            focus: "",
+            focus: null,
             selected: null
         }
 
